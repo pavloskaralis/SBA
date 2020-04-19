d60d0d68b2b0e08333d4f418069252e7b8b0fff6
pavloskaralis <52301933+pavloskaralis@users.noreply.github.com>
2020-04-18 21:21:31 -0400

    undid

--- sba-api/src/main/java/main/MainController.java
+++ sba-api/src/main/java/main/MainController.java
@@ -71,7 +71,7 @@ public class MainController {
 
                         int distance = Levenshtein.calculate(currentWord, entries.get(i).getWord());
 
-                        if(distance < 5) {
+                        if(distance < 4) {
                             Suggestion suggestion = new Suggestion(entries.get(i).getWord(),distance);
                             suggestions.add(suggestion);
                         }
